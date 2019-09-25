ab6add00520bc95a89569adea1aea25849b2313e
Bligh <mackenziebligh@gmail.com>
2019-09-24 20:42:44 -0700

    Updates: 09-24-2019

--- config.fish
+++ config.fish
@@ -6,10 +6,10 @@ if test -n "$EMACS"
 end
 
 # this function may be required
-function my_key_bindings
-    fish_vi_key_bindings
-    bind -M insert -m default kj force-repaint
-end
+# function my_key_bindings
+#     fish_vi_key_bindings
+#     bind -M insert -m default kj force-repaint
+# end
 
 source ~/.fish_aliases
 
@@ -18,8 +18,7 @@ set NVM_DIR $HOME/.nvm
 set -x -g ANT_OPTS '-Dspring.profiles.active=default,local'
 set -x -g SPRING_PROFILES_ACTIVE 'default,local'
 
-set -g fish_key_bindings my_key_bindings
+#set -g fish_key_bindings my_key_bindings
 set -g nvm_dir
 
-set -g theme_nerd_fonts yes
 set -g theme_color_scheme dracula
