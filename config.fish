fffcd7b25b28866dcb8531d7b34d51c89730000e
Bligh <mackenziebligh@gmail.com>
2020-03-06 11:13:39 -0800

    Updates: 03-06-2020

--- config.fish
+++ config.fish
@@ -14,6 +14,9 @@ set PATH /Users/mbligh/.nvm/versions/node/v10.13.0/bin $PATH
 set -x -g ANT_OPTS '-Dspring.profiles.active=default,local'
 set -x -g SPRING_PROFILES_ACTIVE 'default,local'
 
+set -x EDITOR "open -a Emacs"
+set -x VISUAL $EDITOR
+
 #set -g fish_key_bindings my_key_bindings
 set -g nvm_dir
 
